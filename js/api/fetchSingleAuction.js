609ca49ce2485c4540f927b064017bd351f3c603
AlexanderLislelid <alexlislelid@gmail.com>
2026-04-24 15:03:31 +0200

    add image gallery functionality to single listing page

--- js/api/fetchSingleAuction.js
+++ js/api/fetchSingleAuction.js
@@ -2,6 +2,7 @@ import { get } from "./apiClient.js";
 
 const itemId = new URLSearchParams(window.location.search).get("id");
 const container = document.getElementById("listing-container");
+const galleryContainer = document.getElementById("gallery-container");
 
 async function fetchAndRenderListing() {
   try {
@@ -13,22 +14,42 @@ async function fetchAndRenderListing() {
     const listing = data.data;
     const bids = listing.bids;
     const images = listing.media;
+    console.log(images);
 
-    //image -- https://www.w3schools.com/howto/howto_js_tab_img_gallery.asp
-    const gallery = document.createElement("div");
+    //image gallery -- https://www.w3schools.com/howto/howto_js_tab_img_gallery.asp
+    const expandedImg = document.createElement("img");
+    expandedImg.className = "w-full object-cover";
+
+    const thumbnailRow = document.createElement("div");
+    thumbnailRow.className = "flex flex-wrap";
+
     if (images && images.length > 0) {
-      images.forEach((img) => {
-        const image = document.createElement("img");
-        image.src = img.url;
-        image.alt = img.alt;
-        gallery.append(image);
+      expandedImg.src = images[0].url;
+      expandedImg.alt = images[0].alt;
+
+      images.forEach((image) => {
+        const wrapper = document.createElement("div");
+        const thumb = document.createElement("img");
+
+        thumb.src = image.url;
+        thumb.alt = image.alt;
+        thumb.className = "w-full cursor-pointer opacity-60 hover:opacity-100";
+
+        thumb.addEventListener("click", () => {
+          expandedImg.src = image.url;
+          expandedImg.alt = image.alt;
+        });
+
+        wrapper.className = "w-1/4 p-1";
+        wrapper.append(thumb);
+        thumbnailRow.append(wrapper);
       });
     } else {
-      const image = document.createElement("img");
-      image.src = "../images/placeholder.png";
-      image.alt = "";
-      gallery.append(image);
+      expandedImg.src = "/images/placeholder.png";
+      expandedImg.alt = "";
     }
+
+    galleryContainer.append(expandedImg, thumbnailRow);
 
     //title
     const title = document.createElement("h1");
@@ -39,7 +60,8 @@ async function fetchAndRenderListing() {
     description.textContent = listing.description;
 
     //appending
-    container.append(gallery, title, description);
+
+    container.append(title, description);
   } catch (error) {
     console.error("Error fetching listing:", error);
   }
