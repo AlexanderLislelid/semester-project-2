735dba61a6821427abf02137e279c3e3ac385838
AlexanderLislelid <alexlislelid@gmail.com>
2026-04-25 14:04:26 +0200

    update formatMilliseconds function to return HTML structure for time display

--- js/utils/formatter.js
+++ js/utils/formatter.js
@@ -5,5 +5,5 @@ export function formatMilliseconds(ms) {
   const hours = Math.floor((ms / (1000 * 60 * 60)) % 24);
   const days = Math.floor(ms / (1000 * 60 * 60 * 24));
 
-  return `${days}d ${hours}h ${minutes}m`;
+  return `<div class="flex items-center align-center"><p class="font-semibold">${days}</p><p class="text-sm mr-1">d</p><p class="font-semibold">${hours}</p><p class="text-sm mr-1">h</p><p class="font-semibold">${minutes}</p><p class="text-sm mr-1">m</p></div>`;
 }
