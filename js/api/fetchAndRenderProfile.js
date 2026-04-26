31c5270b7bf242ebb06c063f020f755c6e85225f
AlexanderLislelid <alexlislelid@gmail.com>
2026-04-26 14:59:24 +0200

    update profile page layout and implement profile fetching functionality

--- js/api/fetchAndRenderProfile.js
+++ js/api/fetchAndRenderProfile.js
@@ -1,4 +1,19 @@
 import { get, post, put, del } from "./apiClient.js";
 import { showTabs } from "../utils/tabs.js";
+import { loadUser, loadToken } from "../utils/storage.js";
+
+if (!loadUser() && !loadToken()) {
+  window.location.href = "/pages/login.html";
+}
 
 showTabs();
+async function fetchAndRenderProfile() {
+  try {
+    const response = await get(
+      `auction/profiles/${loadUser().name}?_bids=true&_listings=true&_wins=true`,
+    );
+    const data = response.data;
+    console.log(data);
+  } catch (error) {}
+}
+fetchAndRenderProfile();
