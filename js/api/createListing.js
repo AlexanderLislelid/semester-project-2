b78261824d795853fce6a1cfbb76681cd9152c06
AlexanderLislelid <alexlislelid@gmail.com>
2026-04-27 11:55:01 +0200

    implement create listing with media upload functionality and user authentication check

---
+++ js/api/createListing.js
@@ -0,0 +1,77 @@
+import { isLoggedIn } from "../utils/storage.js";
+import { post } from "../api/apiClient.js";
+
+if (!isLoggedIn()) {
+  window.location.href = "/pages/login.html";
+}
+
+const form = document.getElementById("create-listing");
+const title = document.getElementById("title");
+const description = document.getElementById("description");
+const tags = document.getElementById("tags");
+const imageWrapper = document.getElementById("media-wrapper");
+const imageUrl = document.getElementById("media-url");
+const imageAlt = document.getElementById("media-alt");
+const addImgBtn = document.getElementById("media-add");
+const endDate = document.getElementById("listing-end");
+
+const images = [];
+
+addImgBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const url = imageUrl.value;
+  const alt = imageAlt.value;
+
+  const image = { url, alt };
+  images.push(image);
+
+  const item = document.createElement("div");
+  item.className = "flex flex-col items-center gap-2 text-sm";
+
+  const img = document.createElement("img");
+  img.src = url;
+  img.alt = alt;
+  img.className = "h-14 w-14 rounded-md object-cover cursor-pointer";
+
+  const label = document.createElement("p");
+  label.className = "truncate max-w-14";
+  label.textContent = url;
+
+  //remove item (img and urltext) from array by clicking them
+  img.addEventListener("click", () => {
+    const index = images.indexOf(image);
+    images.splice(index, 1);
+    item.remove();
+  });
+
+  item.appendChild(img);
+  item.appendChild(label);
+  imageWrapper.appendChild(item);
+
+  imageUrl.value = "";
+  imageAlt.value = "";
+});
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  try {
+    const body = {
+      title: title.value,
+      description: description.value,
+      tags: tags.value
+        .split(",")
+        .map((t) => t.trim())
+        .filter(Boolean),
+      media: images,
+      endsAt: new Date(endDate.value).toISOString(),
+    };
+    const data = await post("auction/listings", body);
+    // NOTE TO SELF <----- ADD SUCCESS TOAST
+    setTimeout(() => {
+      window.location.href = `/pages/single-listing.html?id=${data.data.id}`;
+    }, 2000);
+  } catch (error) {
+    // NOTE TO SELF <----- ADD ERRORMESSAGE TO USER
+    console.error(error);
+  }
+});
