06e1c3a449c69bca070e24986d9fa3d47b2c64c7
AlexanderLislelid <alexlislelid@gmail.com>
2026-05-04 10:16:57 +0200

    add z index to toast for improved visibility when modal is opened

--- js/components/toasts.js
+++ js/components/toasts.js
@@ -3,7 +3,7 @@ export function showToast(title, message, type) {
 
   if (type === "success") {
     container.innerHTML = `<div
-      class="p-6 fixed top-2.5 left-1/2 -translate-x-1/2 border border-green-500/50 rounded-md gap-2 shadow-md bg-green-100 flex"
+      class="p-6 fixed top-2.5 left-1/2 -translate-x-1/2 border border-green-500/50 rounded-md gap-2 shadow-md bg-green-100 flex z-50"
     >
       <i class="fa-solid fa-circle-check text-3xl text-green-500"></i>
       <div class="text-nowrap">
@@ -13,7 +13,7 @@ export function showToast(title, message, type) {
     </div>`;
   } else if (type === "error") {
     container.innerHTML = `<div
-      class="p-6 fixed top-2.5 left-1/2 -translate-x-1/2 border border-red-500/50 rounded-md gap-2 shadow-md bg-red-100 flex"
+      class="p-6 fixed top-2.5 left-1/2 -translate-x-1/2 border border-red-500/50 rounded-md gap-2 shadow-md bg-red-100 flex z-50"
     >
       <i class="fa-solid fa-circle-xmark text-3xl text-red-500"></i>
       <div class="text-nowrap">
